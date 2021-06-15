634b0277869f84aa6db23ac758a5a14ecb173145
maanesh <maanesh.shri@gmail.com>
2021-06-15 09:22:15 +0530

    mychatapp

---
+++ app/src/main/java/com/maane/chat_app/Models/Status.java
@@ -0,0 +1,30 @@
+package com.maane.chat_app.Models;
+
+public class Status {
+    private String imageUrl;
+    private long timeStamp;
+
+    public Status() {
+    }
+
+    public Status(String imageUrl, long timeStamp) {
+        this.imageUrl = imageUrl;
+        this.timeStamp = timeStamp;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public long getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(long timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+}
---
+++ app/src/main/java/com/maane/chat_app/SplashScreen.java
@@ -0,0 +1,41 @@
+package com.maane.chat_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+import com.maane.chat_app.Activities.PhoneNumberActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private static int SPLASH_TIMER=3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent intent=new Intent(SplashScreen.this, PhoneNumberActivity.class);
+                startActivity(intent);
+                finish();
+
+
+            }
+        },SPLASH_TIMER);
+
+
+
+
+
+    }
+}
